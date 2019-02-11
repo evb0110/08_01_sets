4b4b8ea35b6c9d21bab4d745515d84a46828c990
evb0110 <7149553@gmail.com>
2019-02-11 11:51:38 +0300

    solved

---
+++ src/js/character.js
@@ -0,0 +1,12 @@
+class Character {
+  constructor(name, type, level, health, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.health = health;
+    this.attack = attack;
+    this.defence = defence;
+  }
+}
+
+export default Character;
