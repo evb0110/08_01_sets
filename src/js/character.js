c562315d06ce7de46d645e77fc39489aadd92bfd
evb0110 <7149553@gmail.com>
2019-02-11 17:38:16 +0300

    added jsdoc

--- src/js/character.js
+++ src/js/character.js
@@ -1,4 +1,14 @@
+/** Class representing a character */
 class Character {
+  /**
+   *
+   * @param {name} Character's name
+   * @param {number} Character's type
+   * @param {level} Character's level
+   * @param {health} Character's health
+   * @param {attack} Character's attack
+   * @param {defence} Character's defence
+   */
   constructor(name, type, level, health, attack, defence) {
     this.name = name;
     this.type = type;
